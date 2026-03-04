a46299538ad5abec53f74e36c6059250fb45704f
Lauro Otavio Urbano <lauro@outlook.com.br>
2026-03-03 22:13:54 -0300

    revertendo commits
    reiniciando o projeto, para configurar corretamente o httpclient, ngx-toastr e angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./pages/country-list/country-list').then(m => m.CountryList)
-    },
-    {
-        path: 'details/:code',
-        loadComponent: () => import('./pages/country-detail/country-detail').then(m => m.CountryDetail)
-    },
-    {
-        path: 'saiba-mais',
-        loadComponent: () => import('./pages/saiba-mais/saiba-mais').then(m => m.SaibaMais)
-    },
-    {
-        path: 'not-found',
-        loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound)
-    },
-    {
-        path: '**',
-        redirectTo: 'not-found',
-        pathMatch: 'full'
-    }
+
 
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    RouterModule,
-    MatCardModule
-  ],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Explorador de Países');
 
-  protected readonly menuItems = [
-    {
-      label: 'Lista de Países',
-      route: '/'
-    },
-    {
-      label: 'Saiba Mais',
-      route: '/saiba-mais'
-    }
-  ];
 
 }
