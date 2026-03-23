9e8c26d8f75cd19695f5873e46023ddc1b11872c
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2026-03-23 08:52:09 -0300

    Update country.mapper.ts

--- src/app/core/mappers/country.mapper.ts
+++ src/app/core/mappers/country.mapper.ts
@@ -4,12 +4,12 @@ export function mapCountry(apiCountry: any): Country {
   return {
     name: apiCountry.name.common,
     cca3: apiCountry.cca3,
-    capital: apiCountry.capital?.[0] ?? '',
+    capital?: apiCountry.capital?.[0] ?? '',
     population: apiCountry.population,
     region: apiCountry.region,
     subregion: apiCountry.subregion,
     flags: apiCountry.flags,
-    borders: apiCountry.borders ?? [],
+    borders?: apiCountry.borders ?? [],
     area: apiCountry.area,
     currencies: apiCountry.currencies,
     languages: apiCountry.languages
