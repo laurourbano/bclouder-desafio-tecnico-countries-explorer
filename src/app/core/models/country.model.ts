503719ad2e4a21720b1353056ca7c5dd1a260694
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2026-03-23 08:52:31 -0300

    Update country.model.ts

--- src/app/core/models/country.model.ts
+++ src/app/core/models/country.model.ts
@@ -1,11 +1,11 @@
 export interface Country {
   name: { common: string; official: string; };
   cca3: string;
-  capital: string[];
+  capital?: string[];
   population: number;
   region: string;
   subregion: string;
-  borders: string[];
+  borders?: string[];
   flags: { png: string; svg: string; };
   languages: Record<string, string>;
   currencies: Record<string, { name: string; symbol: string; }>;
